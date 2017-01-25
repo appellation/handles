3c99d8a1617113ee306d7d3f92558bcb21a4e5d9
Will Nelson <nelson.will@live.com>
2017-01-25 09:18:34 -0800

    re-emit command message events on command loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const remit = require('re-emitter');
 const CommandLoader = require('./util/CommandLoader');
 const CommandMessage = require('./util/CommandMessage');
 
@@ -7,6 +7,14 @@ module.exports = config => {
     return (msg, body) => {
         if(msg) {
             const commandMessage = new CommandMessage(loader, msg, body);
+            remit(commandMessage, loader, [
+                'notACommand',
+                'invalidCommand',
+                'commandStarted',
+                'commandFinished',
+                'commandFailed'
+            ]);
+
             return {
                 commandMessage,
                 result: commandMessage.handle()
