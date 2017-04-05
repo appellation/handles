3221e23d8a57756b0ea76ba05a91f52dbdb0021e
appellation <nelson.will@live.com>
2017-04-04 19:18:21 -0700

    wip queued and stored responses

--- src/Response.js
+++ src/Response.js
@@ -1,3 +1,5 @@
+const queue = require('queue');
+
 class Response {
 
     /**
@@ -15,13 +17,54 @@ class Response {
          * @type {TextChannel}
          */
         this.channel = message.channel;
+
+        /**
+         * @type {Message} responseMessage Previously sent responses will be edited.
+         */
+        this.responseMessage = null;
+
+        this._q = queue({
+            autostart: true,
+            concurrency: 1
+        });
     }
 
-    send(data)  {
-        return this.channel.send(data);
+    /**
+     * Send a message using the Discord.js `Message.send` method.  If a prior
+     * response has been sent, it will edit that unless the `force` parameter
+     * is set.
+     * @param {*} data The data to send
+     * @param {boolean} [force=false] Whether to send a new message regardless
+     * of any prior responses.
+     * @returns {Promise<Message>}
+     */
+    send(data, force = false)  {
+        return new Promise((resolve, reject) => {
+            this._q.push(cb => {
+                if(this.responseMessage && !force) {
+                    const edited = this.responseMessage.edit(data);
+                    edited.then((m) => {
+                        cb(null, m);
+                        resolve(m);
+                    }).catch((e) => {
+                        cb(e);
+                        reject(e);
+                    });
+                } else {
+                    this.channel.send(data).then(m => {
+                        this.responseMessage = m;
+                        cb(null, m);
+                        resolve(m);
+                    }).catch(e => {
+                        cb(e);
+                        reject(e);
+                    });
+                }
+            });
+        });
     }
 
-    dm(data)    {
+    dm(data) {
         return this.message.author.sendMessage(data);
     }
 
