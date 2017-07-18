28b9f4d1ab7d17ad45cda410eac5e3ec2fb88c0a
Will Nelson <nelson.will@live.com>
2017-07-17 19:26:33 -0700

    clear directory before building

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
+const del = require('del');
 const ts = require('gulp-typescript');
 const project = ts.createProject('tsconfig.json');
 
 gulp.task('default', () => {
+  del.sync(['dist/**', '!dist']);
   return project.src()
     .pipe(project())
     .js.pipe(gulp.dest('dist'));
