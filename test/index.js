c6a5cb3959f9a797d0899667d1414e4eafe2311a
Will Nelson <nelson.will@live.com>
2017-07-26 07:17:30 -0700

    better raven handling

--- test/index.js
+++ test/index.js
@@ -16,50 +16,85 @@ const handler = new handles.Client({
   prefixes: new Set(['x!'])
 });
 
+handler.on('commandError', ({ command, error }) => {
+  const extra = {
+    message: {
+      content: command.message.content,
+      id: command.message.id,
+      type: command.message.type,
+    },
+    channel: {
+      id: command.message.channel.id,
+      type: command.message.channel.type,
+    },
+    guild: {},
+    client: {
+      shard: command.client.shard ? command.client.shard.id : null,
+      ping: command.client.ping,
+      status: command.client.status,
+    },
+  };
+
+  if (command.message.channel.type === 'text') {
+    extra.guild = {
+      id: command.guild.id,
+      name: command.guild.name,
+      owner: command.guild.ownerID,
+    };
+  }
+
+
+  console.log(raven.captureException(error, {
+    user: {
+      id: command.message.author.id,
+      username: command.message.author.tag,
+    },
+    extra
+  }));
+});
+
 // handler.on('commandFailed', console.error);
 // handler.on('commandError', console.error);
 
-raven.context(() => {
-  client.on('message', m => {
-    const extra = {
-      message: {
-        content: m.content,
-        id: m.id,
-        type: m.type,
-      },
-      channel: {
-        id: m.channel.id,
-        type: m.channel.type,
-      },
-      guild: {},
-      client: {
-        shard: m.client.shard ? m.client.shard.id : null,
-        ping: m.client.ping,
-        status: m.client.status,
-      },
-    };
+client.on('message', m => {
+  // const extra = {
+  //   message: {
+  //     content: m.content,
+  //     id: m.id,
+  //     type: m.type,
+  //   },
+  //   channel: {
+  //     id: m.channel.id,
+  //     type: m.channel.type,
+  //   },
+  //   guild: {},
+  //   client: {
+  //     shard: m.client.shard ? m.client.shard.id : null,
+  //     ping: m.client.ping,
+  //     status: m.client.status,
+  //   },
+  // };
 
-    if (m.channel.type === 'text') {
-      extra.guild = {
-        id: m.guild.id,
-        name: m.guild.name,
-        owner: m.guild.ownerID,
-      };
+  // if (m.channel.type === 'text') {
+  //   extra.guild = {
+  //     id: m.guild.id,
+  //     name: m.guild.name,
+  //     owner: m.guild.ownerID,
+  //   };
 
-      const perms = m.channel.permissionsFor(m.guild.me);
-      extra.channel.permissions = perms ? perms.serialize() : null;
-    }
+  //   const perms = m.channel.permissionsFor(m.guild.me);
+  //   extra.channel.permissions = perms ? perms.serialize() : null;
+  // }
 
-    raven.setContext({
-      user: {
-        id: m.author.id,
-        username: m.author.tag,
-      },
-      extra,
-    });
-
-    handler.handle(m);
-  });
+  // raven.context({
+  //   user: {
+  //     id: m.author.id,
+  //     username: m.author.tag,
+  //   },
+  //   extra,
+  // }, () => {
+  handler.handle(m).catch(() => null);
+  // });
 });
 
 client.once('ready', () => console.log('ready'));
